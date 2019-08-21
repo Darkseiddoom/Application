3fefef7c8a1691c16711c33a0d1856641da0155e
Laxman <laxmanbist07@gmail.com>
2019-08-21 14:39:35 +0530

    Suffering

--- bin/app/App.java
+++ bin/app/App.java
@@ -3,5 +3,6 @@ public class App{
     public static void main(String[] args)
     {
         System.out.println("Hello World, goodbye world and hello death");
+        System.out.println("I am not bored with life, I am just fascinated by death");
     }
 }
