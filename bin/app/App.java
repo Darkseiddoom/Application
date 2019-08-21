655202426b2daa626e052e4c2395b7afd082808f
Laxman <laxmanbist07@gmail.com>
2019-08-21 15:14:40 +0530

    Alternate12

--- bin/app/App.java
+++ bin/app/App.java
@@ -2,7 +2,7 @@
 public class App{
     public static void main(String[] args)
     {
-        System.out.println("Shot right out of the sky");
+        System.out.println("Shot right out of the sky, you're my kryptanite");
         System.out.println("I am not bored with life, I am just fascinated by death");
     }
 }
