b6f359a941df09a79c40ee8c1e98c1a21f620b46
Laxman <laxmanbist07@gmail.com>
2019-08-21 14:34:26 +0530

    Rebirth

--- bin/app/App.java
+++ bin/app/App.java
@@ -2,6 +2,6 @@
 public class App{
     public static void main(String[] args)
     {
-        System.out.println("Hello World, goodbye world and hello death");
+        System.out.println("Hello World, goodbye world and hello darkness my old friend");
     }
 }
