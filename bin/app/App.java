07d43608d61117eaae8b3d2e89e240d78336ee82
Laxman <laxmanbist07@gmail.com>
2019-08-21 15:52:05 +0530

    razzie

--- bin/app/App.java
+++ bin/app/App.java
@@ -5,7 +5,7 @@ public class App{
 
 
         System.out.println("I feel like i am being punished for expecting something to happen, which probably wont happen");
-        System.out.println("I am not bored with life, I am just fascinated with death");
+        System.out.println("I am not bored with life, I am just fascinated with death. We all will fly till the day we die.");
 
     }
 }
